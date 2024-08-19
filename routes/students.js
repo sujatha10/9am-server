30d2fd83825f56a63f54c2adc6a5a7c19d040b5d
sujatha10 <muvva.sujatha1@gmail.com>
2024-08-19 15:29:39 +0530

    latest

--- routes/students.js
+++ routes/students.js
@@ -1,6 +1,9 @@
 var express = require('express')
 var router = express.Router();
-var getDB = require('../common/dbConn')
+var getDB = require('../common/dbConn');
+var mongodb = require('mongodb')
+const objectId = mongodb.ObjectId
+const { route } = require('.');
 
 router.post('/register',async function(req,res,next){
      try {
@@ -25,5 +28,30 @@ router.get('/get-std', async function(req,res,next){
      res.send(ex.message)
  }
 
+})
+router.put('/update-std', async function(req, res,next) {
+     try {
+     const id = req.query.id
+     const data = req.body.data
+     const db = await getDB()
+     const collection = db.collection('students')
+     const result = await collection.updateOne({_id:objectId.createFromHexString(id)},{$set:data})
+     res.send(result)
+     } catch (ex) {
+          console.error(ex)
+          res.send(ex.message)
+     }
+})
+router.delete('/delete-std/:id', async function(req,res,next){
+     try {
+     const id = req.params.id
+     const db = await getDB();
+     const collection = db.collection('students')
+     const result = await collection.deleteOne({_id:objectId.createFromHexString(id)})
+     res.send(result)
+     } catch (ex) {
+          console.error(ex)
+          res.send(ex.message)
+     }
 })
 module.exports = router
