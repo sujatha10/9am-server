77549b2c72f6b5b8fbe3b4f9f629afc2a2734d84
sujatha10 <muvva.sujatha1@gmail.com>
2024-08-20 15:35:08 +0530

    add login endpoint

--- routes/students.js
+++ routes/students.js
@@ -54,4 +54,13 @@ router.delete('/delete-std/:id', async function(req,res,next){
           res.send(ex.message)
      }
 })
+router.post("/login", function(req, res, next){
+    const {uid,pwd} = req.body
+    if (uid == 'nit' && pwd == 'nitnit') {
+       res.send([{uid,pwd}])
+    } else {
+        res.send([])
+    }
+})
+
 module.exports = router
